c6bc4e009d3947bc2d5b2d0cd11f3c3fa8ef5cf8
Zdenek Styblik <stybla@turnovfree.net>
2011-07-09 10:40:26 +0200

    MOD print -> printf resp. output msg formatting Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>
    Signed-off-by: Zdeněk Styblík <stybla@turnovfree.net>

--- ftp.script.pl
+++ ftp.script.pl
@@ -15,7 +15,11 @@ sub help {
 
 sub purifyFName {
 	my $self = $_;
-	my $string = shift;
+	my $string = shift || undef;
+	if (!$string)
+	{
+		return "";
+	}
 	my @charArr = split(//, $string);
 	my $newStr = '';
 
@@ -42,22 +46,22 @@ sub purifyFName {
 	while (1) {
 		# what's the length of new string?
 		if (length($newStr) < 1 && $newStr =~ /^[\.]+$/) {
-			print "String is too short, $string\n";
+			printf("String is too short '%s'\n", $string);
 			last;
 		} # if length $newStr
 		# do we have rw to $string ?
 		if (! -w $string && ! -w './') {
-			print "No write perm, $string\n";
+			printf("No write permission to '%s'\n", $string);
 			last;
 		} # if ! -w $string
-		print $string.":".$newStr."\n";
+		printf("%s:%s\n", $string, $newStr);
 		if ($string eq $newStr) {
-			print "Files are the same [$string:$newStr]\n";
+			printf("Files are the same [%s:%s]\n", $string, $newStr);
 			last;
 		}
 		# if $string =~ $newStr
 		print "Will rename $string -> $newStr\n";
-		rename($string, $newStr) or die "Couldn't rename f/d, $!\n";
+		rename($string, $newStr) or die("Couldn't rename f/d, '$!'\n");
 		last;
 	} # while 1
 	return $newStr;
@@ -66,8 +70,8 @@ sub purifyFName {
 sub scanDir {
 	my $self = $_;
 	my $dir2scan = shift;
-	chdir($dir2scan) or die "Couldn't change dir, $!\n";
-	opendir (DIR, '.') or die "Couldn't open directory, $!\n";
+	chdir($dir2scan) or die("Couldn't change dir, '$!'\n");
+	opendir (DIR, '.') or die("Couldn't open directory, '$!'\n");
 	my @files = grep { !/^\.{1,2}$/ } readdir (DIR);
 	closedir DIR;
 	foreach my $file (@files) {
@@ -76,7 +80,7 @@ sub scanDir {
 				next;
 			} # if $file eq +upload
 			my $newFile = &purifyFName($file);
-			print "Diving into $newFile\n";
+			printf("Diving into '%s'\n", $newFile);
 			&scanDir($newFile);
 		} else {
 			&purifyFName($file);
@@ -96,7 +100,7 @@ for my $argument (@ARGV) {
 		my $dir = basename($argument);
 		chdir($cwd);
 		my $newDirName = &purifyFName($dir);
-		print "Going to scan $newDirName\n";
+		printf("Going to scan '%s'\n", $newDirName);
 		&scanDir($newDirName);
 	} else {
 		print &purifyFName($argument)."\n";
